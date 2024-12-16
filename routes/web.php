7b550d9168405fca10fbbe441f1500b02618deb0
TesyaKell <220711896@students.uajy.ac.id>
2024-12-16 19:47:58 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReservasiKonsultasiController;
 use App\Models\PembelianObat;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ObatController;
 use App\Http\Controllers\TransaksiController;
-use App\Models\PembelianObat;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
